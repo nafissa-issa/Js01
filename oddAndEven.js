2c25f5094d8d8030da80fa7524b7514481612860
nafissa-issa <124690665+nafissa-issa@users.noreply.github.com>
2023-02-13 16:38:50 +0100

    Add files via upload

---
+++ oddAndEven.js
@@ -0,0 +1,11 @@
+Number(20);
+
+function Number(i)
+{
+    for(let a = 0; a <= i ; a++)
+    {
+      if(a%2===0) console.log("Impaire");
+      
+      else console.log("Paire");
+    }
+}
---
+++ weekendsVerified.js
@@ -0,0 +1,6 @@
+ let day = "lundi";
+ if (day=="samedi"|| day=="dimanche") 
+ console.log("Weekend");
+else
+ console.log("Weekday");
+    
